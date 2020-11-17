b924043ef8fa080fbdc534a98fd23092b5b4410e
pyagmyrov <permanyagmyrov@gmail.com>
2020-11-17 14:08:58 +0200

    edited settings.py

--- bookie/settings.py
+++ bookie/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1','206.189.221.52',]
 # Application definition
 
 INSTALLED_APPS = [
-    'bookie',
+    'core',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
