79bdc1f33cc2f561d0599851f07038357a130433
pyagmyrov <permanyagmyrov@gmail.com>
2020-11-17 14:13:10 +0200

    edited setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/pyagmyrov/rest'
+PROJECT_GIT_URL='https://github.com/pyagmyrov/rest.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/rest'
 
